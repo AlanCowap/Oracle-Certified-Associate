6efaf44b1852bb773fe3ab4e269d507a8478f6df
Alan Cowap <spam@alancowap.com>
2015-06-25 12:43:59 +0100

    Initial commit

---
+++ OCJA_2015/src/com/alancowap/ocja/sample/HelloWorld.java
@@ -0,0 +1,20 @@
+/** 
+ *  Simple class to demo java.
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+
+package com.alancowap.ocja.sample;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
