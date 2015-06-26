f83d59bd28471d699b01ba0dbcdfbeec8acd3df8
Alan Cowap <spam@alancowap.com>
2015-06-26 09:45:26 +0100

    Add Person class

---
+++ OCJA_2015/src/com/alancowap/ocja/sample/Person.java
@@ -0,0 +1,21 @@
+/** 
+ *  Simple class to represent a Person.
+ *  
+ *  We could improve this class by using access modifiers
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+
+package com.alancowap.ocja.sample;
+
+public class Person {
+
+	int age;
+	String name;
+	char gender;
+	
+	
+}
