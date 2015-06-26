c676b598b3fb3c15a88c18b4cc408461239b0d50
Alan Cowap <spam@alancowap.com>
2015-06-26 10:51:14 +0100

    Create and interact with a Person object

--- OCJA_2015/src/com/alancowap/ocja/sample/HelloWorld.java
+++ OCJA_2015/src/com/alancowap/ocja/sample/HelloWorld.java
@@ -14,6 +14,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		Person p = new Person();
+		System.out.println(p.getName());
+		p.setName("Alan");
+		System.out.println(p.getName());
 
 	}
 
