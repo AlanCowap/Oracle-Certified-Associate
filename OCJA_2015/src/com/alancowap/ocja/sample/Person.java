362060d7de3d8a83b9031a30e58e3c9ce42afdca
Alan Cowap <spam@alancowap.com>
2015-06-26 10:25:01 +0100

    Add getters and setters

--- OCJA_2015/src/com/alancowap/ocja/sample/Person.java
+++ OCJA_2015/src/com/alancowap/ocja/sample/Person.java
@@ -1,7 +1,7 @@
 /** 
  *  Simple class to represent a Person.
  *  
- *  We could improve this class by using access modifiers
+ *  We could improve this class by using access modifiers for instance variables
  *  
  * @author Alan Cowap 
  * @version 1.0  
@@ -13,9 +13,30 @@ package com.alancowap.ocja.sample;
 
 public class Person {
 
+	//State
 	int age;
 	String name;
 	char gender;
+	
+	//Behaviour
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
 	
 	
 }
