7b42e3a5647aa6a99a060a4c900e4f036a05fd62
Alan Cowap <spam@alancowap.com>
2015-06-29 14:27:06 +0100

    Sample exam question

---
+++ OCJA_2015/src/com/alancowap/ocja/questions/sample/Buddy.java
@@ -0,0 +1,24 @@
+/** 
+ *  OCJA Sample exam question
+ *  Label, breaks and continue keywords
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+package com.alancowap.ocja.questions.sample;
+
+public class Buddy {
+	public static void main(String[] args) {
+		
+		def:
+			for(short s = 1; s < 7; s++) {
+				if(s == 4) break def;
+				if(s == 1) continue;
+				System.out.print(s + ".");
+			}
+	}
+}
+//What is the output?
