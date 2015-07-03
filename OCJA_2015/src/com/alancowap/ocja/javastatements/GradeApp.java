dde94df9489b4ca4947f78f92f9fc33df5914105
Alan Cowap <spam@alancowap.com>
2015-07-03 09:17:17 +0100

    Add switch statement

--- OCJA_2015/src/com/alancowap/ocja/javastatements/GradeApp.java
+++ OCJA_2015/src/com/alancowap/ocja/javastatements/GradeApp.java
@@ -1,10 +1,12 @@
 /** 
  *  Class to demonstrate if, else-if, else statements
+ *  Demonstrate uise of switch statement
  *  Also uses Scanner class to get user input
  *  
  *  
  * @author Alan Cowap 
- * @version 1.0  
+ * @version 1.0  Initial version
+ * @version 1.1  Add switch statement
  * @dependencies None
  *  
  */
@@ -15,17 +17,17 @@ import java.util.Scanner;
 public class GradeApp {
 
 	public static void main(String[] args) {
-		
+
 		// get a result from the user		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please enter a percentage (0-100) ");
 		int result = scan.nextInt(); //assume we get a valid input
-		
 
-		
-		// determine a grade
+
+
+		// determine a grade using if
 		String grade = "Unknown";
-		
+
 		if(result > 84 ){
 			grade = "A";
 		}else if(result > 69){
@@ -37,8 +39,20 @@ public class GradeApp {
 		}else{
 			grade = "Fail";
 		}		
-		
+
 		System.out.println("Your grade is " + grade);
+
+		//determine a result using switch
+		String resultRange = "Undetermined";
+		switch (grade){
+			case "A":	resultRange = "85-100"; break;
+			case "B":	resultRange = "70-84"; break;
+			case "C":	resultRange = "55-69"; break;
+			case "D":	resultRange = "40-54"; break;
+			default :	resultRange = "fail";
+		}
+		System.out.println("Your result range is " + resultRange);
+
 	}
 
 }
