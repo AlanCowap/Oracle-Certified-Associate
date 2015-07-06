77bc8377a84adb5352fb096bdb94a3428fa1455f
Alan Cowap <spam@alancowap.com>
2015-07-06 10:51:46 +0100

    Demo for loop

---
+++ OCJA_2015/src/com/alancowap/ocja/javastatements/NameApp.java
@@ -0,0 +1,28 @@
+/** 
+ *  Simple class to demonstrate for-loop.
+ *  
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+package com.alancowap.ocja.javastatements;
+
+
+public class NameApp {
+
+
+	public static void main(String[] args) {
+		System.out.println("Alan Cowap");
+		for(int i=0; i < 100; ++i){
+			System.out.println(i + "Alan");
+		}
+			
+		
+
+		
+	}
+
+}
