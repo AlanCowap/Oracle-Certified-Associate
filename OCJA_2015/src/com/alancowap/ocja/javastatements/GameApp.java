3dde2a366b2b49c90f4daf9f8942aca9d5fc32dc
Alan Cowap <spam@alancowap.com>
2015-07-09 09:22:49 +0100

    Add user input

--- OCJA_2015/src/com/alancowap/ocja/javastatements/GameApp.java
+++ OCJA_2015/src/com/alancowap/ocja/javastatements/GameApp.java
@@ -1,6 +1,6 @@
 /** 
  *  Simple class to demonstrate while loop.
- *  
+ *  Take user input
  *  
  *  
  * @author Alan Cowap 
@@ -10,18 +10,34 @@
  */
 package com.alancowap.ocja.javastatements;
 
+import java.util.Scanner;
+
 public class GameApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		//do while loop
 		
-		int lives = 3;
+		// display menu
+		System.out.println("1. How many games to you want to play (0 quits)?");
+		
+		// get user choice
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Please enter your choice > ");
+		int userChoice = scan.nextInt();
+		
+		// act based on user choice
+		System.out.println("You chose " + userChoice);
+		
+		
+		// while loop
+		int lives = userChoice;
 		while(lives > 0){
 			System.out.println("Playing game...");
 			lives--;
 		}
 		System.out.println(lives + " lives left, Game over");
 
+		
 	}
 
 }
