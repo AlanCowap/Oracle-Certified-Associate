8f993fb8684913a92ea7c39c3646d3309ce9d380
Alan Cowap <spam@alancowap.com>
2015-07-09 12:55:12 +0100

    Demo continue statement

--- OCJA_2015/src/com/alancowap/ocja/javastatements/ContinueApp.java
+++ OCJA_2015/src/com/alancowap/ocja/javastatements/ContinueApp.java
@@ -1,6 +1,6 @@
 /** 
  *  Demo continue statement
- *  
+ *   Prettify the output
  *  
  *  
  * @author Alan Cowap 
@@ -14,9 +14,18 @@ public class ContinueApp {
 
 	public static void main(String[] args) {
 		//output the odd numbers in the range 10..100
-		for(int i=10; i<101; ++i){
-			if((i%2)==0) {continue;}
-			else {System.out.println(i);}
+		for(int i=10, oddCount=0; i<101; ++i){
+			if( (i%2) == 0) {	// even numbers
+				continue;		// don't execute the lines below, continue with next iteration
+			}
+			else {				// odd numbers
+				//Print a new line, on every 10th odd number
+				if( (oddCount % 10) == 0){
+					System.out.println("");
+				}
+				++oddCount;
+				System.out.print(i + " ");
+			}
 		}
 	}
 
