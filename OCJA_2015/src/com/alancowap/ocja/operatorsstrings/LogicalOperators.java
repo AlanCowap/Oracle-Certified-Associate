93c2775e34b17420b242c562aacaabd5b69c5ead
Alan Cowap <spam@alancowap.com>
2015-07-13 12:36:03 +0100

    Demo logical operators: && || !

--- OCJA_2015/src/com/alancowap/ocja/operatorsstrings/LogicalOperators.java
+++ OCJA_2015/src/com/alancowap/ocja/operatorsstrings/LogicalOperators.java
@@ -22,7 +22,7 @@ public class LogicalOperators {
 		System.out.println(lo.state);
 		lo.changeState();
 		System.out.println(lo.state);
-		if(! lo.state){
+		if( ! lo.state ){
 			System.out.println("State is false");
 		}else{
 			System.out.println("State is true");
