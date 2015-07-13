7f1c2ddf38ca025d022fd3ecad63aa6505e591d0
Alan Cowap <spam@alancowap.com>
2015-07-13 09:51:25 +0100

    Demo pre and post increment operator

---
+++ OCJA_2015/src/com/alancowap/ocja/codechallenge/operatorsstrings/Incrementor.java
@@ -0,0 +1,31 @@
+/** 
+ *  Demo pre and post increment operator
+ *  
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+package com.alancowap.ocja.codechallenge.operatorsstrings;
+
+
+public class Incrementor {
+
+	
+	public static void main(String[] args) {
+		//Post-increment
+		int age = 0;
+		System.out.println("Age is " + age++);		// 0		
+		System.out.println("Second age is " + age);	// 1
+		
+		//Pre-increment
+		int numPeople = 99;
+		System.out.println("\nNum people is: " + ++numPeople);		//100
+		System.out.println("Second num people is: " + numPeople);	//100
+		
+
+	}
+
+}
