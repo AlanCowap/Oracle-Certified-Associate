296283b792798ec1b6e28f58a484c7ccd8c47cf0
Alan Cowap <spam@alancowap.com>
2015-07-13 12:22:19 +0100

    Demo logical operators: && || !

---
+++ OCJA_2015/src/com/alancowap/ocja/operatorsstrings/LogicalOperators.java
@@ -0,0 +1,38 @@
+/** 
+ *  Demo logical operators: && || !
+ *  
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+package com.alancowap.ocja.operatorsstrings;
+
+
+public class LogicalOperators {
+
+	private boolean state = false;
+	
+	public static void main(String[] args) {
+		LogicalOperators lo = new LogicalOperators();
+		System.out.println(lo.state);
+		lo.changeState();
+		System.out.println(lo.state);
+		lo.changeState();
+		System.out.println(lo.state);
+		if(! lo.state){
+			System.out.println("State is false");
+		}else{
+			System.out.println("State is true");
+		}
+		
+	}
+	
+	private void changeState(){
+		state = !state;
+	}
+	
+
+}
