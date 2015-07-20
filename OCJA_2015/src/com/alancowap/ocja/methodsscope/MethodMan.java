09835dbcadf1aa607a00c10bb29e2d684dc443f6
Alan Cowap <spam@alancowap.com>
2015-07-20 09:06:41 +0100

    Demo class definitions

---
+++ OCJA_2015/src/com/alancowap/ocja/methodsscope/MethodMan.java
@@ -0,0 +1,32 @@
+/** 
+ *  Demo method definitions
+ *  
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+package com.alancowap.ocja.methodsscope;
+
+public class MethodMan {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+	
+	public String getName(){
+		return "Albert";
+	}
+	public String getDept(){
+		return "R&D";
+	}
+	
+	// more methods
+
+} //end class
+
+//can’t have methods outside of the class :(
+
