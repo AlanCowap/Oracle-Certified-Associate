da0d8c1ff873cc857e1846d17f9736a84acc021e
Alan Cowap <spam@alancowap.com>
2015-07-31 10:49:43 +0100

    Initial commit

---
+++ OCJA_2015/src/com/alancowap/ocja/objectoriented/Objecto.java
@@ -0,0 +1,22 @@
+/** 
+ *  Demo creating an object
+ *  
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+package com.alancowap.ocja.objectoriented;
+
+
+public class Objecto {
+
+
+	public static void main(String[] args) {
+		new Objecto();
+
+	}
+
+}
