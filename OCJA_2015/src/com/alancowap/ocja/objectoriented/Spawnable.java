6596485cbaab52eacc4ed41d6b43e97a5a1ae4c1
Alan Cowap <spam@alancowap.com>
2015-08-06 10:33:32 +0100

    Rules for interface implicitnessness

--- OCJA_2015/src/com/alancowap/ocja/objectoriented/Spawnable.java
+++ OCJA_2015/src/com/alancowap/ocja/objectoriented/Spawnable.java
@@ -10,8 +10,13 @@
  */
 package com.alancowap.ocja.objectoriented;
 
+//interfaces are implicitly abstract
 public abstract interface Spawnable {
 	
+	//"variables" in interfaces are implicitly public static final
+	public static final int NUM_SPAWNS = 123;
+	
+	//methods in interfaces are implicitly public abstract
 	public abstract void spawn();
 
 }
