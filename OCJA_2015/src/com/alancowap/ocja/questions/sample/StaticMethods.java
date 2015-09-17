ce8a648e6ebe7726536aa8925e371fccef707f35
Alan Cowap <spam@alancowap.com>
2015-09-17 15:48:38 +0100

    Demo static call to Parent class method thrgugh a casted child class object reference, thx to Kevin Phair for the suggestion.

--- OCJA_2015/src/com/alancowap/ocja/questions/sample/StaticMethods.java
+++ OCJA_2015/src/com/alancowap/ocja/questions/sample/StaticMethods.java
@@ -4,7 +4,8 @@
  *  
  *  
  * @author Alan Cowap 
- * @version 1.0  
+ * @version 1.0 Initial code  
+ * @version 1.1  Demo static call to Parent class method thrgugh a casted child class object reference, thx to Kevin Phair for the suggestion  
  * @dependencies None
  *  
  */
@@ -22,15 +23,19 @@ public class StaticMethods {
 		Bottom b = new Bottom();
 		b.runMe();
 		b.num = 8;
+		
+		((Top)b).runMe(); //This will execute runMe() in Top, even if there was another in Bottom
 	}
 
 }
 
 class Top{
 	static int num = 10;
-	static void runMe(){}
+	static void runMe(){System.out.println("Top");}
 }
 
 class Middle extends Top{}
 
-class Bottom extends Middle{}
+class Bottom extends Middle{
+	static void runMe(){System.out.println("Bottom");}
+}
