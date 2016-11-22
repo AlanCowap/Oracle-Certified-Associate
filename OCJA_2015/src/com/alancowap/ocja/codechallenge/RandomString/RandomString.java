ef93265fb1e3628350c31be5ee982e7d0c823b40
AlanCowap <spam@alancowap.com>
2016-11-22 14:45:27 +0000

    First iteration of RandomString Code challenge

---
+++ OCJA_2015/src/com/alancowap/ocja/codechallenge/RandomString/RandomString.java
@@ -0,0 +1,36 @@
+/** 
+ *  Demo Random
+ *  
+ *  
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+package com.alancowap.ocja.codechallenge.RandomString;
+
+import java.util.Random;
+
+/**
+ * @author user
+ *
+ */
+public class RandomString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("RandomStringApp");
+		Random rnd = new Random();
+		
+		for (int i = 0; i < 50; i++) {
+			int num = rnd.nextInt(25);
+			char ch = (char) (num + 'a');
+			System.out.printf("%d<%c>", num, ch);
+		}
+	}
+
+}
