ad7c8eeb6006c75499d4d609d909ce124628e27d
AlanCowap <spam@alancowap.com>
2017-02-14 15:48:22 +0000

    Add file for command line compilation

---
+++ OCJA_2015/src/com/alancowap/ocja/basics/CommandLine.java
@@ -0,0 +1,11 @@
+package com.alancowap.ocja.commandline;
+
+public class CommandLine{
+	public static void main(String[] args){
+		System.out.println("Hello command line");
+	}
+}
+
+// javac -d classes CommandLine.java
+// java -cp classes com.alancowap.ocja.commandline.CommandLine
+// ensure the classes directory exists first
