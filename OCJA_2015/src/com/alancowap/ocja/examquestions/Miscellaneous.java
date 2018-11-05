8ff11ea3b7fb5c1d4ad7c387b67c3aea3a96fd5b
AlanCowap <spam@alancowap.com>
2018-11-05 11:16:51 +0000

    Add Miscellaneous.java - incl. Wrapper Classes Equality

---
+++ OCJA_2015/src/com/alancowap/ocja/examquestions/Miscellaneous.java
@@ -0,0 +1,67 @@
+/** 
+ * Sample code based on mock-exam questions.
+ * - Wrapper classes. == versus equals()   
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ * 
+ */
+package com.alancowap.ocja.examquestions;
+
+public class Miscellaneous {
+
+	public static void main(String[] args) {
+		Miscellaneous m = new Miscellaneous();
+		m.wrapperEquality();
+
+	}
+
+	private void wrapperEquality() {
+		System.out.println("Wrapper Equality\n");
+		Number n = 5;	//strange but true (Number is superclass of numeric wrapper classes)
+		//5 is an int, wrapped to an Integer, polymorphic reference from Number ORV
+		//Character and Boolean both extend Object directly
+		System.out.println("instanceof:");
+		System.out.println(n instanceof Integer);	//true
+		System.out.println(n instanceof Byte);		//false
+		Number nd = 5.5;
+		System.out.println(nd instanceof Double);	//true
+		System.out.println(nd instanceof Float);	//false
+		
+		System.out.println("\nequals():");
+		System.out.println(n.equals(new Integer(5)));//true
+		//System.out.println(n.equals(new Byte(5))); //No. constructor "Byte(int)" doesn't exist
+		System.out.println(n.equals(new Byte("5"))); //false 
+		
+		Byte b = 127;
+		Short s = 127;
+		System.out.println("\n== equality operator");
+		//System.out.println(b == s); //No. Incompatible operands: Byte, Short
+		System.out.println((byte) b == s); //true. unwrap, compare
+		System.out.println((byte) ++b == s); //false. unwrap, increment, compare
+		System.out.println(b); //watch for wrap-around (-128)
+		s = 300;
+		
+		System.out.println("\nHeap vs Constant Pool");
+		Integer i = 300;
+		//System.out.println(s == i); //No. Incompatible operands again.
+		System.out.println((short) s == i); //true. unwrap, compare.
+		Integer j = new Integer(300);
+		Integer k = new Integer(300);
+		Integer l = 300;
+		System.out.println(i == j); //false
+		System.out.println(k == j); //false
+		System.out.println(i == l); //false
+		
+		Integer m = 127; //numeric literal in range of Byte => object constant pool
+		Integer p = 127;
+		System.out.println(m == p); //true
+		m++; p++;	//unwrap, increment, wrap.
+		System.out.println(m == p); //false - now outside of Constant Pool
+		
+		
+	}
+	
+}
